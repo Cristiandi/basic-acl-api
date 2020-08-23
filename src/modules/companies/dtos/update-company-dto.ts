961617530cced469e1358a89267e290c548bc1a4
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-23 23:23:56 +0000

    feat(api): deleting all

--- src/modules/companies/dtos/update-company-dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'
-
-import { CreateCompanyDto } from './create-company.dto'
-
-const partialCreateCompanyDto = PartialType<CreateCompanyDto>(CreateCompanyDto)
-
-export const updateCompanyDto = partialCreateCompanyDto
-
-export class UpdateCoffeeDto extends PartialType(CreateCompanyDto) {}
--- src/modules/firebase-admin/firebase-admin.constants.ts
+++
@@ -1,4 +0,0 @@
-export const countryCodesPhoneNumber: any = {
-  CO: '+57',
-  MX: '+52'
-}
