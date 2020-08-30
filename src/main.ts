d51e5329afdea728b44ce986ab3600b173748393
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-30 20:27:14 +0000

    feat: little advance

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import helmet from 'fastify-helmet';
 
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import {
   FastifyAdapter,
   NestFastifyApplication
 } from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
@@ -20,16 +22,25 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   
   // enabling for cors policy
-  app.enableCors();
+  // app.enableCors();
 
   // use helmet
   await app.register(helmet, { hidePoweredBy: false });
-
+  
+  // using the filters
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Setting up Swagger document 
+  const options = new DocumentBuilder()
+    .setTitle('Basic')
+    .setDescription('The basic ACL api description.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const PORT = configService.get<number>('config.app.port');
   const ENVIRONMENT = configService.get<string>('config.environment');
-
 
   await app.listen(PORT, '0.0.0.0');
   
