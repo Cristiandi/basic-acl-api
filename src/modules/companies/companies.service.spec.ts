5e68f1ed06d588a391a6ad52a9d0cf52b03c1711
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2020-08-30 21:27:29 +0000

    feat: create company service unit tests

--- src/modules/companies/companies.service.spec.ts
+++ src/modules/companies/companies.service.spec.ts
@@ -1,4 +1,4 @@
-import { NotFoundException } from '@nestjs/common';
+import { NotFoundException, HttpException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -6,12 +6,15 @@ import { Repository } from 'typeorm';
 import { Company } from './company.entity';
 
 import { CompaniesService } from './companies.service';
+import { CreateCompanyInput } from './dto/create-company-input.dto';
 
 // function to mock the repositry
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 const createMockRepository = <T = any>(): MockRepository<T> => ({
+  create: jest.fn(),
+  find: jest.fn(),
   findOne: jest.fn(),
-  create: jest.fn()
+  save: jest.fn()
 });
 
 describe('CompanyService', () => {
@@ -36,6 +39,36 @@ describe('CompanyService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    describe('when create a company', () => {
+      it('should return the company object', async () => {
+        const expectedCompany = {} as CreateCompanyInput;
+
+        companyRepository.find.mockReturnValue([]);
+        companyRepository.create.mockReturnValue(expectedCompany);
+        companyRepository.save.mockResolvedValue(expectedCompany);
+
+        const company = await service.create(expectedCompany);
+
+        expect(company).toEqual(expectedCompany);
+      });
+    });
+
+    describe('when a company already exists', () => {
+      it('should throw the "HttpException"', async () => {
+        const createCompanyInput = {} as CreateCompanyInput;
+
+        companyRepository.find.mockReturnValue([{}]);
+
+        try {
+          await service.create(createCompanyInput);
+        } catch (error) {
+          expect(error).toBeInstanceOf(HttpException);
+        }
+      });
+    });
   });
 
   describe('findOne', () => {
