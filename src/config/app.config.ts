85e1760db329dc1178b7356355fd5aa0ba072b2d
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2020-09-22 19:31:05 -0500

    feat: adding script

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('config', () => ({
   environment: process.env.NODE_ENV || 'local',
   app: {
-    port: parseInt(process.env.APP_PORT, 10) || 8080,
+    port: parseInt(process.env.PORT, 10) || 8080,
     apiKey: process.env.API_KEY,
     maxHitsAllowed: parseInt(process.env.MAX_HITS_ALLOWED) || 1,
     maxHitsTimeRange: parseInt(process.env.MAX_HITS_TIME_RANGE) || 60
