b3a591818ca32e2cd09d81daca0339b8c93c2477
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2020-09-29 20:04:51 -0500

    feat: confirmation email configs ready

--- src/modules/confirmation-email-configs/confirmation-email-configs.controller.ts
+++ src/modules/confirmation-email-configs/confirmation-email-configs.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ConfirmationEmailConfigsService } from './confirmation-email-configs.service';
+import { CreateConfirmationEmailCionfig } from './dto/create-confirmation-email-config-input.dto';
+import { FindAllConfirmationEmailConfigsParamInput } from './dto/find-all-confirmation-email-configs-param-input.dto';
+import { FindAllConfirmationEmailConfigsQueryInput } from './dto/find-all-confirmation-email-configs-query-input.dto';
+import { FindOneConfirmationEmailConfigInput } from './dto/find-one-confirmation-email-config-input.dto';
+import { UpdateConfirmationEmailConfigInput } from './dto/update-confirmation-email-config-input.dto';
 
+@UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 @Controller('confirmation-email-configs')
-export class ConfirmationEmailConfigsController {}
+export class ConfirmationEmailConfigsController {
+  constructor(private readonly confirmationEmailConfigsService: ConfirmationEmailConfigsService) {}
+
+  @Post()
+  create (@Body() createConfirmationEmailCionfig: CreateConfirmationEmailCionfig): Promise<any> {
+    return Promise.resolve(createConfirmationEmailCionfig);
+  }
+
+  @Get(':companyUuid')
+  findAll(
+    @Param() findAllConfirmationEmailConfigsParamInput: FindAllConfirmationEmailConfigsParamInput,
+    @Query() findAllConfirmationEmailConfigsQueryInput: FindAllConfirmationEmailConfigsQueryInput
+  ): Promise<any> {
+    return Promise.resolve({
+      ...findAllConfirmationEmailConfigsParamInput,
+      ...findAllConfirmationEmailConfigsQueryInput
+    });
+  }
+
+  @Patch(':companyUuid/:id')
+  update(
+    @Param() findOneConfirmationEmailConfigInput: FindOneConfirmationEmailConfigInput,
+    @Body() updateConfirmationEmailConfigInput: UpdateConfirmationEmailConfigInput
+  ): Promise<any> {
+    return Promise.resolve({
+      ...findOneConfirmationEmailConfigInput,
+      ...updateConfirmationEmailConfigInput
+    });
+  }
+
+  @Delete(':companyUuid/:id')
+  remove(@Param() findOneConfirmationEmailConfigInput: FindOneConfirmationEmailConfigInput): Promise<any> {
+    return Promise.resolve(findOneConfirmationEmailConfigInput);
+  }
+}
