3fabd9250549b16ad487930e791dc2792cee087f
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-02-03 22:57:35 -0500

    feat: tags for docs

--- src/modules/assigned-roles/dto/get-user-assigned-roles-input.dto.ts
+++ src/modules/assigned-roles/dto/get-user-assigned-roles-input.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsUUID } from 'class-validator';
 
 export class GetUserAssignedRolesInput {
+  @ApiProperty()
   @IsUUID()
   readonly companyUuid: string;
 
+  @ApiProperty()
   @IsString()
   readonly authUid: string;
 }
