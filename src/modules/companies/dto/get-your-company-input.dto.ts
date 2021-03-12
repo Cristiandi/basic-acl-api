189b785cb79be542243b4fb5c88a61559e40179d
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-03-11 21:26:10 -0500

    fix: fixing eslint

--- src/modules/companies/dto/get-your-company-input.dto.ts
+++ src/modules/companies/dto/get-your-company-input.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUUID } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class GetYourCompanyInput {
   @ApiProperty()
